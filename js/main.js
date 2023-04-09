86d7ab19a5c7f8a7c03747e788ceabdff93aa979
sindystar <hasungah@gmail.com>
2023-04-09 13:20:02 +0900

    함수 패키징

--- js/main.js
+++ js/main.js
@@ -2,20 +2,24 @@ const btns = document.querySelectorAll("ul li");
 const boxs = document.querySelectorAll("section article");
 
 //btn에 반복을 돌면서 (일반for문, forEach문)
-btns.forEach((el, index) => {
+btns.forEach((el, ind) => {
   //클릭 이벤트를 부여한다 
   el.addEventListener("click", (e) => {
     //a태그의 링크이벤트는 막고
     e.preventDefault();
 
-    //버튼에 반복을 돌면서 모두 on을 지우고 
-    for (let btn of btns) btn.classList.remove("on");
-    //클릭한 인덱스의 버튼만 on을 붙인다 
-    btns[index].classList.add("on");
+    //재이벤트 방지 구문 (ison) contains사용
+    let isOn = e.currentTarget.classList.contains("on");
+    if (isOn) return;
 
-    //박스에 반복을 돌면서 모두 on을 지우고 
-    for (let box of boxs) box.classList.remove("on");
-    //클릭한 인덱스의 박스만 on을 붙인다 
-    boxs[index].classList.add("on");
+    activation(btns, ind);
+    activation(boxs, ind);
   })
-})
+});
+
+//할성화 함수를 만듦
+function activation(arr, index) {
+  for (let box of arr) box.classList.remove("on");
+  arr[index].classList.add("on");
+}
+//위의 공통 부분에서, 변화 되는 부분을 매개변수로 받을수 있도록 만들면서 클을 제작 => 활성화 함수 
